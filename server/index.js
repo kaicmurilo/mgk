05e515667d06f96b16d0d8f6c1804c34671e211a
Kaic <kaicmurilo@live.com>
2022-10-08 10:04:03 -0400

    finsh

--- server/index.js
+++ server/index.js
@@ -11,6 +11,15 @@ app.use(cors())
 const posts = require('./routes/api/post')
 app.use('/api/posts', posts)
 
+// handle production
+if (process.env.NODE_ENV === 'production') {
+    //static folder
+    app.use(express.static(__dirname + '/public/'))
+
+    //handle spa
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server inciou em porta ${port}`))
