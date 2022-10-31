6751772a5a692055e24e61748d5de558a3684a20
Kaic <kaicmurilo@live.com>
2022-10-31 17:08:58 -0400

    iniciando jogo ping pong

--- server/public/js/about.d405f3c8.js
+++ server/public/js/about.d405f3c8.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[443],{776:function(n,e,t){t.r(e),t.d(e,{default:function(){return d}});var c=t(252),r=t(949);const u={class:"home"},l=(0,c._)("img",{alt:"Vue logo",src:r},null,-1),s=[l];function i(n,e){return(0,c.wg)(),(0,c.iD)("div",u,s)}var a=t(744);const o={},f=(0,a.Z)(o,[["render",i]]);var d=f}}]);
-//# sourceMappingURL=about.7ad96310.js.map
+//# sourceMappingURL=about.d405f3c8.js.map
